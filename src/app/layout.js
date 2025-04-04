83e36d71c4aaeacacfd3a30794104023d68e0f6d
Ayush Anshuman Supakar <supakarayush@gmail.com>
2025-04-04 21:48:09 +0530

    Resume Updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
